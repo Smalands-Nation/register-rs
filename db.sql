75f984ae4feeba9d72953286500a25018b024099
axel paulander <axel.paulander@gmail.com>
2021-11-23 23:02:30 +0100

    Also update db.sql

--- db.sql
+++ db.sql
@@ -1,7 +1,8 @@
 create table if not exists menu (
 	name text not null primary key,
 	price integer not null,
-	available boolean default true not null
+	available boolean default true not null,
+	special boolean default false not null
 );
 
 create table if not exists receipts (
@@ -18,3 +19,14 @@ create table if not exists receipt_item (
 	foreign key(item) references menu(name),
 	primary key(receipt, item)
 );
+
+create view if not exists receipts_view as
+	select receipts.time, receipt_item.item, receipt_item.amount, menu.price, menu.special, receipts.method
+	from receipts
+		inner join receipt_item on receipts.time = receipt_item.receipt
+		inner join menu on receipt_item.item = menu.name;
+
+insert or ignore into menu (name, price, available, special)
+	values
+		('Special', 1, true, true),
+		('Rabatt', -1, true, true);
