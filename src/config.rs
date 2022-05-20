38915271e56e09ee39cbd733dbc32d5888f0b962
axel paulander <axel.paulander@gmail.com>
2022-05-20 08:12:39 +0200

    Add db migrations

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,29 @@
-use {crate::error::Result, rusqlite::Connection};
+use {
+    crate::error::Result,
+    lazy_static::lazy_static,
+    rusqlite::Connection,
+    rusqlite_migration::{Migrations, M},
+};
+
+lazy_static! {
+    static ref MIGRATIONS: Migrations<'static> = Migrations::new(vec![
+        M::up(include_str!("../db.sql")),
+        M::up(
+            r#"
+                ALTER TABLE receipt_item ADD COLUMN price INTEGER DEFAULT 1 NOT NULL;
+                UPDATE receipt_item SET price = (
+                    SELECT price FROM menu WHERE receipt_item.item = menu.name
+                );
+                DROP VIEW receipts_view;
+                CREATE VIEW IF NOT EXISTS receipts_view AS
+                    SELECT receipts.time, receipt_item.item, receipt_item.amount, receipt_item.price, menu.special, receipts.method 
+                    FROM receipts
+                        INNER JOIN receipt_item ON receipts.time = receipt_item.receipt
+                        INNER JOIN menu ON receipt_item.item = menu.name;
+            "#
+        ),
+    ]);
+}
 
 pub fn init_db() -> Result<Connection> {
     match dirs::config_dir() {
@@ -12,8 +37,10 @@ pub fn init_db() -> Result<Connection> {
                 },
             };
             conf_path.push("db.db");
-            let conn = Connection::open(conf_path)?;
-            conn.execute_batch(include_str!("../db.sql"))?;
+            let mut conn = Connection::open(conf_path)?;
+
+            MIGRATIONS.to_latest(&mut conn)?;
+
             Ok(conn)
         }
         None => Err("No config dir".into()),
